507bc4b2ebb0a650740c018e755e2e66f7f5ddc7
Mohamed Thahir <thahirsprojects@gmail.com>
2024-12-19 16:55:33 -0800

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: HeroBiz
-* Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+* Template Name: homeBiz
+* Template URL: https://bootstrapmade.com/homebiz-bootstrap-business-template/
 * Updated: Aug 07 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
